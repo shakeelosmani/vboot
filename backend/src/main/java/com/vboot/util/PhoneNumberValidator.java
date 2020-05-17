334605c5021c01d57a580025cfa606155b76d27f
Shakeel Osmani <shakeel.osmani@gmail.com>
2020-05-17 16:18:20 -0400

    phone number validator

---
+++ backend/src/main/java/com/vboot/util/PhoneNumberValidator.java
@@ -0,0 +1,29 @@
+package com.vboot.util;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.vboot.Interface.Phone;
+
+public class PhoneNumberValidator implements ConstraintValidator<Phone, PhoneNumber> {
+
+  @Override
+  public void initialize(Phone constraintAnnotation) {
+
+  }
+
+  @Override
+  public boolean isValid(PhoneNumber phoneNumber, ConstraintValidatorContext context) {
+    if (phoneNumber.getLocale() == null || phoneNumber.getValue() == null) {
+      return false;
+    }
+    try {
+      PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
+      return phoneNumberUtil.isValidNumber(phoneNumberUtil.parse(phoneNumber.getValue(), phoneNumber.getLocale()));
+    } catch (NumberParseException e) {
+      return false;
+    }
+  }
+}
