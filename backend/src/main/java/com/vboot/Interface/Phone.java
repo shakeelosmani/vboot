6bb038436512b039168cbc387dd1f98c8b89a3eb
Shakeel Osmani <shakeel.osmani@gmail.com>
2020-05-17 16:17:39 -0400

    custom validator interface

---
+++ backend/src/main/java/com/vboot/Interface/Phone.java
@@ -0,0 +1,20 @@
+package com.vboot.Interface;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+import com.vboot.util.PhoneNumberValidator;
+
+@Documented
+@Constraint(validatedBy = PhoneNumberValidator.class)
+@Target({ ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Phone {
+  String locale() default "";
+
+  String message() default "Invalid phone number";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
+}
